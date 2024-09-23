907ad88074863b0e1aaafeb5afc52d2cc12e1bc7
BryanPanchiZapata <bryanlife2530@gmail.com>
2024-09-23 12:31:43 -0500

    Reto 13 - Taller1

--- Taller1/src/clearminds/cuentas/Cuenta.java
+++ Taller1/src/clearminds/cuentas/Cuenta.java
@@ -4,12 +4,51 @@ public class Cuenta {
 	private String id;
 	private String tipo = "A";
 	private double saldo;
-	
-	//Continuar con los Construtores
-	
 
-	
-	// Los getter y setter ya estan definidos
+	// Construtores
+	public Cuenta(String id) {
+		this.id = id;
+	}
+
+	public Cuenta(String id, String tipo, double saldo) {
+		this.id = id;
+		this.tipo = tipo;
+		this.saldo = saldo;
+	}
+
+	// Método para imprimir
+	public void imprimir() {
+		System.out.println("****************");
+		System.out.println("");
+		System.out.println("CUENTA");
+		System.out.println("");
+		System.out.println("****************");
+		System.out.println("");
+		System.out.println("Numero de cuenta: " + this.id);
+		System.out.println("Tipo: " + this.tipo);
+		System.out.println("Saldo: " + this.saldo);
+		System.out.println("");
+		System.out.println("****************");
+	}
+
+	// Método imprimirConMiEstilo
+	public void imprimirConMiEstilo() {
+		System.out.println("==========================");
+		System.out.println("           BANCO          ");
+		System.out.println("==========================");
+		System.out.println("     ESTADO DE CUENTA     ");
+		System.out.println("--------------------------");
+		System.out.println("");
+		System.out.println("Numero de cuenta: " + this.id);
+		System.out.println("Tipo: " + this.tipo);
+		System.out.println("Saldo: " + this.saldo);
+		System.out.println("");
+		System.out.println("--------------------------");
+		System.out.println("GRACIAS POR SU CONFIANZA :)");
+		System.out.println("==========================");
+	}
+
+	// Getter and Setter
 	public String getId() {
 		return id;
 	}
--- Taller1/src/clearminds/cuentas/test/TestCuenta.java
+++ Taller1/src/clearminds/cuentas/test/TestCuenta.java
@@ -5,8 +5,57 @@ import clearminds.cuentas.Cuenta;
 public class TestCuenta {
 
 	public static void main(String[] args) {
-		Cuenta c1=new Cuenta();
+		Cuenta cuenta1 = new Cuenta("03476"); // Instanciar el objeto de la clase cuenta cuenta1
+		Cuenta cuenta2 = new Cuenta("03476", "C", 98); // Instanciar el objeto de la clase Cuenta cuenta2
+		Cuenta cuenta3 = new Cuenta("03476"); // Instanciar el objeto de la clase Cuenta cuenta3
+		Cuenta cuenta4 = new Cuenta("0987"); // Instanciar el objeto de la clase Cuenta cuenta4
+		Cuenta cuenta5 = new Cuenta("0557", "C", 10); // Instanciar el objeto de la clase Cuenta cuenta5
+		Cuenta cuenta6 = new Cuenta("0666"); // Instanciar el objeto de la clase Cuenta cuenta6
 
+		// Setter modificar valor cuenta1
+		cuenta1.setSaldo(675);
+
+		// Setter modificar valor cuenta3
+		cuenta3.setTipo("C");
+
+		// Setter modificar valores cuenta4
+		cuenta4.setSaldo(10);
+
+		// Mostrar los valores
+		System.out.println("--------Valores iniciales--------");
+		cuenta1.imprimir();
+		System.out.println("");
+		cuenta2.imprimir();
+		System.out.println("");
+		cuenta3.imprimir();
+		System.out.println("");
+
+		// Setter modificar valor cuenta1
+		cuenta1.setSaldo(444);
+
+		// Setter modificar valor cuenta2
+		cuenta2.setTipo("D");
+
+		// Setter modificar valor cuenta3
+		cuenta3.setSaldo(597);
+
+		// Imprimir valores
+		System.out.println("--------Valores Modificados-------");
+		cuenta1.imprimir();
+		System.out.println("");
+		cuenta2.imprimir();
+		System.out.println("");
+		cuenta3.imprimir();
+		System.out.println("");
+
+		// Imprimir valores
+		System.out.println("--------Valores con método imprimirConMiEstilo-------");
+		cuenta4.imprimirConMiEstilo();
+		System.out.println("");
+		cuenta5.imprimirConMiEstilo();
+		System.out.println("");
+		cuenta6.imprimirConMiEstilo();
+		System.out.println("");
 	}
 
 }
