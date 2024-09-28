6f936b30114e3522d3d2dc59d997c673860f4eb8
BryanPanchiZapata <bryanlife2530@gmail.com>
2024-09-27 19:22:27 -0500

    Reto 17 clase TestBuscarProductoNull

---
+++ EvaluaciónBPZ/src/com/clearminds/test/TestBuscarProductoNull.java
@@ -0,0 +1,23 @@
+package com.clearminds.test;
+
+import com.clearminds.componentes.Producto;
+import com.clearminds.maquina.MaquinaDulces;
+
+public class TestBuscarProductoNull {
+	public static void main(String[] args) {
+		MaquinaDulces maquina=new MaquinaDulces();
+		maquina.configurarMaquina("A1", "A2", "B1", "B2");
+		
+		Producto producto=new Producto("KE34","Papitas",0.85);
+		maquina.cargarProducto(producto, "B1", 4);
+		
+		Producto prod=maquina.buscarProductoEnCelda("A1");
+		System.out.println("Producto encontrado:"+prod.getNombre());
+		//El error esta en la linea 16, en la variable prod ya que se esta intentando
+		//llamar al metodo getNombre pero en no existen los productos.
+		//Se definio que si no existian los preductos nos retornaba en null.
+		
+		
+		
+	}
+}
